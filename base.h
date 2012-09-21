32949aeee3217d85b20a7efb440d0a0ff8a30bc2
bloodead <Alex_callegari@hotmail.fr>
2012-09-21 19:44:24 +0200

    AJOUT SYSTEME BRICK --> COLLI DESTROY SHOW

--- base.h
+++ base.h
@@ -1,6 +1,19 @@
 #ifndef _base_h_
 #define _base_h_
 
+
+typedef struct	s_brick
+{
+	int	x;
+	int	y;
+}	t_brick;
+
+typedef struct	s_mur
+{
+	t_brick	brick;
+	struct	s_mur* next;
+}	t_mur;
+
 typedef struct	s_balle
 {
 	int	x;
@@ -26,6 +39,7 @@ typedef struct	s_env
 	t_barre barre;
 }	t_env;
 
+void	init_mur(t_mur* mur, t_env* env);
 void	init_barre(t_env* env);
 void	actua_barre(t_env* env, int direct);
 void	move_barre(t_env* env);
--- main.c
+++ main.c
@@ -6,7 +6,27 @@
 #include "base.h"
 
 
-void	check_wall(t_env* env)
+
+void	destroy_brick(t_mur* mur, t_env* env)
+{
+	int	x;
+	int	count;
+
+	count = 0;
+	x = mur->brick.x;
+	while (count != 4)
+	{
+		tputs(tgoto(env->cm, x, mur->brick.y), 1, id_put);
+		write(1, " ", 1);
+		count = count + 1;
+		x = x + 1;
+	}
+	 mur->brick.y = 0;
+	 mur->brick.x = 0;
+
+}
+
+void	check_wall(t_env* env, t_mur* mur)
 {
 	int	x;
 	int	y;
@@ -23,24 +43,37 @@ void	check_wall(t_env* env)
 		env->balle.addy = -1;
 	if (y == env->barre.y  && x >= env->barre.x && x <= env->barre.size)
 		env->balle.addy = -1;
+	while (mur->next != 0)
+	{
+			if (y == mur->brick.y)
+			if (x >= mur->brick.x && x <= mur->brick.x + 3)
+			{
+				destroy_brick(mur, env);
+				env->balle.addy = 1;
+				break;
+			}
+				mur = mur->next;
+	}
+		
 }
 
-int	init(t_env* env)
+int	init(t_env* env, t_mur* mur)
 {
 	if (init_env(env))
 		return (1);
 	init_cadre(env);
+	init_mur(mur, env);
 	init_balle(env);
 	init_barre(env);
 }
 
-int	run(t_env* env)
+int	run(t_env* env, t_mur* mur)
 {
 	sleep(10);
 	while (1)
 	{
 		move_barre(env);
-		check_wall(env);
+		check_wall(env, mur);
 		move_balle(env);
 	}
 	return 0;
@@ -49,7 +82,8 @@ int	run(t_env* env)
 int	main(void)
 {
 	t_env	env;
+	t_mur	mur;
 
-	init(&env);
-	run(&env);
+	init(&env, &mur);
+	run(&env, &mur);
 }
